9acae727101b2096e6ff10efb0dfa9b03bd11fa6
Raymond Wang <raymond.cr.wang@gmail.com>
2024-06-05 17:50:39 -0400

    improved email sending script

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ Clone this repo
 1. Put your OpenAI key in the .env file. Use the template.
 2. Filter and download your table for sponsorships. Follow the example shown here. For spon: go to the sponsorship tracker and download as CSV. Delete the rows you don't care about on the first page. Put it in the src folder and name it spon.csv.
 3. Run the spon.ipynb notebook.
-4. Run the email_sender.py script. (only for Mac users with Outlook desktop app)
+4. Run the email_sender.py script. (only for Mac users with Outlook desktop app) | You might want to temporarily set your default sender email as your hackwestern email after you login with it to Outlook.
 
 Remember to install all dependencies. Check requirements.txt
 
--- email_sender.py
+++ email_sender.py
@@ -1,27 +1,75 @@
 import pandas as pd
 import mistune
 from appscript import app, k
+from mactypes import Alias
+from pathlib import Path
 
 # Read the CSV into pandas
 df = pd.read_csv("output/spon_output.csv")
 
 
-def draft_email(company, title, email, body_html):
-    outlook = app("Microsoft Outlook")
-    msg = outlook.make(
-        new=k.outgoing_message,
-        with_properties={
-            k.subject: title,
-            k.content: body_html,
-        },
-    )
+class Outlook(object):
+    def __init__(self):
+        self.client = app("Microsoft Outlook")
 
-    msg.make(
-        new=k.recipient,
-        with_properties={k.email_address: {k.address: email}},
-    )
-    msg.open()
-    msg.activate()
+
+class Message(object):
+    def __init__(
+        self, parent=None, subject="", body="", to_recip=[], cc_recip=[], show_=True
+    ):
+        if parent is None:
+            parent = Outlook()
+        client = parent.client
+
+        self.msg = client.make(
+            new=k.outgoing_message,
+            with_properties={k.subject: subject, k.content: body},
+        )
+
+        self.add_recipients(emails=to_recip, type_="to")
+        self.add_recipients(emails=cc_recip, type_="cc")
+
+        if show_:
+            self.show()
+
+    def show(self):
+        self.msg.open()
+        self.msg.activate()
+
+    def add_attachment(self, p):
+        # p is a Path() obj, could also pass string
+        p = Alias(str(p))  # convert string/path obj to POSIX/mactypes path
+        attach = self.msg.make(new=k.attachment, with_properties={k.file: p})
+
+    def add_recipients(self, emails, type_="to"):
+        if not isinstance(emails, list):
+            emails = [emails]
+        for email in emails:
+            self.add_recipient(email=email, type_=type_)
+
+    def add_recipient(self, email, type_="to"):
+        msg = self.msg
+
+        if type_ == "to":
+            recipient = k.to_recipient
+        elif type_ == "cc":
+            recipient = k.cc_recipient
+
+        msg.make(new=recipient, with_properties={k.email_address: {k.address: email}})
+
+
+def draft_email(company, title, email, body_html):
+    subject = title
+    body = body_html
+    to_recip = [email]
+
+    msg = Message(subject=subject, body=body, to_recip=to_recip)
+
+    # Attach the file
+    p = Path("src/sponpkg.pdf")
+    msg.add_attachment(p)
+
+    msg.show()
 
 
 # Iterate through each row of the dataframe, and call the draft_email function
